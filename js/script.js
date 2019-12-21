d0af643aac4e59bd00949f99547a20f3ad378706
oluwanisola <ohksam@gmail.com>
2019-12-21 21:16:48 +0100

    nnn

--- js/script.js
+++ js/script.js
@@ -42,6 +42,19 @@ $("#block-slider").owlCarousel({
 
 
 
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
 
 
 //------------------------------------- Skills percentage setup------------------------------------------------//
